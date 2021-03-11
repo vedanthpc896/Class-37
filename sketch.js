b8a50137a01ffa67e7811faf48b703fe58047e0c
Programmer_King <72929704+vedanthpc896@users.noreply.github.com>
2021-03-11 23:07:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canva;
+var gameState=0;
+var contestantCount;
+var database;
+var quiz;
+var question;
+var contestant;
+
+function setup(){
+  canvas = createCanvas(850,400);
+
+  
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
